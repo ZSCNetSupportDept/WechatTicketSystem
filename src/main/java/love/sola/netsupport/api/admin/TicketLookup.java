d518492caaa63b5dbee236b5c6961b5159bd12aa
Sola <loli@sola.love>
2015-12-16 16:38:17 +0800

    init

--- src/main/java/love/sola/netsupport/api/admin/TicketLookup.java
+++ src/main/java/love/sola/netsupport/api/admin/TicketLookup.java
@@ -1,10 +1,18 @@
 package love.sola.netsupport.api.admin;
 
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
 /**
  * ***********************************************
  * Created by Sola on 2015/12/13.
  * Don't modify this source without my agreement
  * ***********************************************
  */
-public class TicketQuery {
+@WebServlet(name = "TicketLookup", urlPatterns = "/api/admin/ticketlookup", loadOnStartup = 24)
+public class TicketLookup extends HttpServlet {
+
+	
+
+
 }
