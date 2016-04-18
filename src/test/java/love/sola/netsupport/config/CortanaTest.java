1e18bc3179423506862155c14405879dbf4bf1af
Sola <loli@sola.love>
2016-04-18 16:18:50 +0800

    cortana test

---
+++ src/test/java/love/sola/netsupport/config/CortanaTest.java
@@ -0,0 +1,18 @@
+package love.sola.netsupport.config;
+
+import org.junit.Test;
+
+/**
+ * ***********************************************
+ * Created by Sola on 2016/4/18.
+ * Don't modify this source without my agreement
+ * ***********************************************
+ */
+public class CortanaTest {
+
+	@Test
+	public void load() throws Exception {
+		Cortana.load();
+	}
+
+}
