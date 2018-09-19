ecf741007f55524866531e06004a9dba182147ae
Sola <dev@sola.love>
2018-09-19 14:16:08 +0800

    fix URLEncodeTest
    Signed-off-by: Sola <dev@sola.love>

--- src/test/java/love/sola/netsupport/util/URLEncodeTest.java
+++ src/test/java/love/sola/netsupport/util/URLEncodeTest.java
@@ -25,7 +25,7 @@ public class URLEncodeTest {
                         .title("Test Title")
                         .msg("Test Message")
                         .toString(),
-                equalTo("http://s.wts.sola.love/nm/v2/result.html?type=1&title=Test%20Title&msg=Test%20Message&")
+                equalTo("http://wwbx.zsc.edu.cn/nm/v2/result.html?type=1&title=Test%20Title&msg=Test%20Message&")
         );
     }
 
